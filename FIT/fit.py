0c284e7878a8999c3ffbb0d6988e9b3998a75dd4
Beola0 <57176681+Beola0@users.noreply.github.com>
2019-11-12 17:57:11 +0100

    minor changes

--- FIT/fit.py
+++ FIT/fit.py
@@ -66,7 +66,8 @@ ax2 = fig.add_subplot(122)
 ax2.set_title(r'Contour plot')
 ax2.plot(m_g.values['mu'],m_g.values['sigma'],'k.',markersize=3.)
 level = np.array([1,4,9,16,25]) 
-cs = ax2.contour(mu,sigma,chi,level) 
+chi_appo = chi - chi.min()
+cs = ax2.contour(mu,sigma,chi_appo,level) 
 ax2.set_xlabel(r'$\mu$')
 ax2.set_ylabel(r'$\sigma$')
 cbar = fig.colorbar(cs)
@@ -83,7 +84,7 @@ for x in x_asimov:
     n += 1
 
 chi_sq_gp = Chi_Squared(Gaussian,x_poisson,bins)
-params0_p = np.ndarray((2,),buffer=np.array([0.1,1.2])) # mu and sigma
+params0_p = np.ndarray((2,),buffer=np.array([0.,1.])) # mu and sigma
 
 m_p = Minuit(chi_sq_gp.chi_squared_stat_minuit,mu=params0_p[0],sigma=params0_p[1],pedantic=False) #fix_mu = True 
 m_p.migrad()
@@ -139,11 +140,42 @@ ax4.grid()'''
 time_el_c = time.perf_counter_ns()
 print('\nelapsed time: ' + str(time_el_c*10**(-6)) + ' ms')
 
-
 plt.ion()
 plt.show()
 
+# test of mu and sigma distribution
 
+dim = 1000
+mu_array = np.zeros(dim)
+sigma_array = np.zeros(dim)
+
+for n0 in np.arange(0,dim):
+    x_poisson = np.ndarray(len(x_asimov))
+    n=0
+    for x in x_asimov:
+        x_poisson[n] = np.random.poisson(x,1)
+        n += 1
+    chi_sq_ = Chi_Squared(Gaussian,x_poisson,bins)
+    m_ = Minuit(chi_sq_.chi_squared_stat_minuit,mu=params0_p[0],sigma=params0_p[1],pedantic=False) 
+    m_.migrad()
+    mu_array[n0] = m_.values['mu']
+    sigma_array[n0] = m_.values['sigma']
+
+fig5 = plt.figure(figsize=[12.5, 7.5])
+
+ax5 = fig5.add_subplot(121)
+ax5.set_title(r'$\mu$ distribution')
+ax5.hist(mu_array,25,color='b',histtype='step')
+ax5.set_xlabel(r'$\mu$')
+ax5.set_ylabel(r'N')
+ax5.grid()
+
+ax6 = fig5.add_subplot(122)
+ax6.set_title(r'$\sigma$ distribution')
+ax6.hist(sigma_array,25,color='r',histtype='step')
+ax6.set_xlabel(r'$\sigma$')
+ax6.set_ylabel(r'N')
+ax6.grid()
 
 
 
